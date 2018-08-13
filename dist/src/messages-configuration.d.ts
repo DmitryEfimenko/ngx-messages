3d64a1bb342354d1121806daa06b58396e63dd65
DmitryEfimenko <dmitrief@gmail.com>
2018-08-12 19:54:40 -0700

    clean

--- dist/src/messages-configuration.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IMessagesConfiguration {
-    showErrorsOnlyIfInputDirty: boolean;
-}
-export declare class MessagesConfiguration implements IMessagesConfiguration {
-    showErrorsOnlyIfInputDirty: boolean;
-}
--- dist/src/messages-configuration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var MessagesConfiguration = (function () {
-    function MessagesConfiguration() {
-        this.showErrorsOnlyIfInputDirty = true;
-    }
-    return MessagesConfiguration;
-}());
-exports.MessagesConfiguration = MessagesConfiguration;
-//# sourceMappingURL=messages-configuration.js.map
